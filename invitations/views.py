be6b5880ca51b7dec57bb8dd865c6075160406c8
Carlos <carlos@queo.pt>
2019-09-04 12:25:39 +0100

    Error handling greatly improved

--- invitations/views.py
+++ invitations/views.py
@@ -23,7 +23,7 @@ InviteForm = get_invite_form()
 
 
 class SendInvite(FormView):
-    template_name = 'invitations/forms/_invite.html'
+    template_name = "invitations/forms/_invite.html"
     form_class = InviteForm
 
     @method_decorator(login_required)
@@ -41,59 +41,51 @@ class SendInvite(FormView):
         except Exception:
             return self.form_invalid(form)
         return self.render_to_response(
-            self.get_context_data(
-                success_message=_('%(email)s has been invited') % {
-                    "email": email}))
+            self.get_context_data(success_message=_("%(email)s has been invited") % {"email": email})
+        )
 
     def form_invalid(self, form):
         return self.render_to_response(self.get_context_data(form=form))
 
 
 class SendJSONInvite(View):
-    http_method_names = [u'post']
+    http_method_names = [u"post"]
 
     @method_decorator(login_required)
     def dispatch(self, request, *args, **kwargs):
         if app_settings.ALLOW_JSON_INVITES:
-            return super(SendJSONInvite, self).dispatch(
-                request, *args, **kwargs)
+            return super(SendJSONInvite, self).dispatch(request, *args, **kwargs)
         else:
             raise Http404
 
     def post(self, request, *args, **kwargs):
         status_code = 400
         invitees = json.loads(request.body.decode())
-        response = {'valid': [], 'invalid': []}
+        response = {"valid": [], "invalid": []}
         if isinstance(invitees, list):
             for invitee in invitees:
                 try:
                     validate_email(invitee)
                     CleanEmailMixin().validate_invitation(invitee)
                     invite = Invitation.create(invitee)
-                except(ValueError, KeyError):
+                except (ValueError, KeyError):
                     pass
-                except(ValidationError):
-                    response['invalid'].append({
-                        invitee: 'invalid email'})
-                except(AlreadyAccepted):
-                    response['invalid'].append({
-                        invitee: 'already accepted'})
-                except(AlreadyInvited):
-                    response['invalid'].append(
-                        {invitee: 'pending invite'})
-                except(UserRegisteredEmail):
-                    response['invalid'].append(
-                        {invitee: 'user registered email'})
+                except (ValidationError):
+                    response["invalid"].append({invitee: "invalid email"})
+                except (AlreadyAccepted):
+                    response["invalid"].append({invitee: "already accepted"})
+                except (AlreadyInvited):
+                    response["invalid"].append({invitee: "pending invite"})
+                except (UserRegisteredEmail):
+                    response["invalid"].append({invitee: "user registered email"})
                 else:
                     invite.send_invitation(request)
-                    response['valid'].append({invitee: 'invited'})
+                    response["valid"].append({invitee: "invited"})
 
-        if response['valid']:
+        if response["valid"]:
             status_code = 201
 
-        return HttpResponse(
-            json.dumps(response),
-            status=status_code, content_type='application/json')
+        return HttpResponse(json.dumps(response), status=status_code, content_type="application/json")
 
 
 class AcceptInvite(SingleObjectMixin, View):
@@ -111,54 +103,37 @@ class AcceptInvite(SingleObjectMixin, View):
     def post(self, *args, **kwargs):
         self.object = invitation = self.get_object()
 
-        # Compatibility with older versions: return an HTTP 410 GONE if there
-        # is an error. # Error conditions are: no key, expired key or
-        # previously accepted key.
-        if app_settings.GONE_ON_ACCEPT_ERROR and \
-                (not invitation or
-                 (invitation and (invitation.accepted or
-                                  invitation.key_expired()))):
-            return HttpResponse(status=410)
-
-        # No invitation was found.
         if not invitation:
-            # Newer behavior: show an error message and redirect.
             get_invitations_adapter().add_message(
-                self.request,
-                messages.ERROR,
-                'invitations/messages/invite_invalid.txt')
+                self.request, messages.ERROR, "invitations/messages/invite_invalid.txt"
+            )
             return redirect(app_settings.LOGIN_REDIRECT)
 
-        # The invitation was previously accepted, redirect to the login
-        # view.
         if invitation.accepted:
             get_invitations_adapter().add_message(
                 self.request,
                 messages.ERROR,
-                'invitations/messages/invite_already_accepted.txt',
-                {'email': invitation.email})
-            # Redirect to login since there's hopefully an account already.
+                "invitations/messages/invite_already_accepted.txt",
+                {"email": invitation.email},
+            )
             return redirect(app_settings.LOGIN_REDIRECT)
 
-        # The key was expired.
         if invitation.key_expired():
             get_invitations_adapter().add_message(
-                self.request,
-                messages.ERROR,
-                'invitations/messages/invite_expired.txt',
-                {'email': invitation.email})
-            # Redirect to sign-up since they might be able to register anyway.
-            return redirect(self.get_signup_redirect())
+                self.request, messages.ERROR, "invitations/messages/invite_expired.txt", {"email": invitation.email}
+            )
+            return redirect(app_settings.LOGIN_REDIRECT)
+
+        if self.request.user.is_authenticated():
+            messages.error(self.request, "Someone is already logged in. Logout first.")
+            return redirect(app_settings.LOGIN_REDIRECT)
 
         # The invitation is valid.
         # Mark it as accepted now if ACCEPT_INVITE_AFTER_SIGNUP is False.
         if not app_settings.ACCEPT_INVITE_AFTER_SIGNUP:
-            accept_invitation(invitation=invitation,
-                              request=self.request,
-                              signal_sender=self.__class__)
+            accept_invitation(invitation=invitation, request=self.request, signal_sender=self.__class__)
 
-        get_invitations_adapter().stash_verified_email(
-            self.request, invitation.email)
+        get_invitations_adapter().stash_verified_email(self.request, invitation.email)
 
         return redirect(self.get_signup_redirect())
 
@@ -181,18 +156,14 @@ def accept_invitation(invitation, request, signal_sender):
     invite_accepted.send(sender=signal_sender, email=invitation.email)
 
     get_invitations_adapter().add_message(
-        request,
-        messages.SUCCESS,
-        'invitations/messages/invite_accepted.txt',
-        {'email': invitation.email})
+        request, messages.SUCCESS, "invitations/messages/invite_accepted.txt", {"email": invitation.email}
+    )
 
 
 def accept_invite_after_signup(sender, request, user, **kwargs):
     invitation = Invitation.objects.filter(email__iexact=user.email).first()
     if invitation:
-        accept_invitation(invitation=invitation,
-                          request=request,
-                          signal_sender=Invitation)
+        accept_invitation(invitation=invitation, request=request, signal_sender=Invitation)
 
 
 if app_settings.ACCEPT_INVITE_AFTER_SIGNUP:
